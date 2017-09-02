679574b59df783c1f69f1e0ff4a03542a3257930
Sparticle999 <alexscruton@gmail.com>
2017-09-02 21:13:41 +0100

    Update something.js

--- something.js
+++ something.js
@@ -1,1 +1,1 @@
-
+function reverse(str) { return str.split("").reverse().join(""); } function simpleencode(str){ string='' for(var n = 1; n < len(str)-1; n++){ newWord = newWord + word[n]; } return newWord +word[0]+"ay"; } function encode(str,n){ if(n==1){ return simpleencode(str) } else{ return encode(str,n-1) } }
